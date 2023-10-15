65556cf96902ec4e678c0c8017cd86dd44fb09ca
byhuang1998 <byhuang1998@gmail.com>
2023-10-15 13:46:41 +0800

    master constraint

---
+++ src/CONSTRAINT/constraint.sql
@@ -0,0 +1,21 @@
+-- primary key 主键
+-- unique 唯一
+-- auto_increment 自增
+-- check 检查
+-- default 默认
+-- not null 非空
+
+create table test.user (
+                           id int primary key auto_increment comment '主键',
+                           name varchar(10) not null unique comment '姓名',
+                           age int check ( age < 120 and age > 0 ) comment '年龄',
+                           status char(1) default '1' comment '状态',
+                           gender char(1) comment '性别'
+) comment '用户表';
+
+insert into test.user (name, age, status, gender) values ('zs', 21, '1', '男');
+insert into test.user (name, age, gender) values ('ls', 21, '男'); -- default约束
+insert into test.user (name, age, gender) values ('ls', 121, '男'); -- 违反age字段的check约束
+
+select * from test.user;
+
